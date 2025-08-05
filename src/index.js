395ec4825d7eb390c846268b76c0e38924e81796
Xumanth <juturusumanth23@gmail.com>
2025-08-05 18:49:13 +0530

    Format the code with prettier

--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import "./styles.css";
 
 const button = document.querySelector("button");
 const nav = document.querySelector("nav");
-const hidden = document.querySelector("ul")
-button.addEventListener("click", toggleNav)
+const hidden = document.querySelector("ul");
+button.addEventListener("click", toggleNav);
 function toggleNav() {
-  hidden.className = (hidden.className == "hidden") 
-                   ? "shown" 
-                   : "hidden";
+  hidden.className = hidden.className == "hidden" ? "shown" : "hidden";
 }
